75efb8780c81d5ff4a6b700c7aec8e03a36d572f
irubikcube <cristian.canessa@mayor.cl>
2025-04-09 09:29:03 -0400

    Mejora del menú y mensajes de la calculadora PyCalc para mayor claridad y corrección de errores tipográficos.

--- PyCalc V.0.2.py
+++ PyCalc V.0.2.py
@@ -1,33 +1,39 @@
-while True:  
-    ###Menu Calculadora
-    print ("SELECCIONA LA OPERACIÓN")
-    print("1 Sumar")
-    print("2 Restar")
-    print("3 Mulpiplicar")
-    print("4 Dividir")
-    print ("5 Exit")
-    print ("Nombra una opcion")
+"""
+Nuestra calculadora realiza operaciones basicas tales como suma, resta, multiplicación y división.
+El programa permite al usuario elegir la operación deseada y luego ingresar los números para realizar el cálculo.
 
-    #### try hara lo que se indique que esta solcitando
+"""
+
+while True:  
+    #Menu de PyCalc
+    print("Favor seleccione una Operación")
+    print("1. Quiero Sumar")
+    print("2. Quiero Restar")
+    print("3. Quiero Mulpiplicar")
+    print("4. Deseo Dividir")
+    print("5. Es todo gracias, Salir")
+    print("Selecciona una opcion")
+
+    #El comando "try" hara lo que se indique que se pida
     try:
-        operacion = int(input("Selecciona una operación (1-5): "))
+        operacion = int(input("Favor selecciona una operación (1-5): "))
 
         if operacion == 5:
-            print("Gracias por usar la Calculadora.")
+            print("Gracias por usar PyCalc!")
             break
         elif operacion in [1, 2, 3, 4]:
-            x = float(input("Ingresa n1: "))
-            y = float(input("Ingresa n2: "))
+            x = float(input("Ingresa X1: "))
+            y = float(input("Ingresa X2: "))
 
             if operacion == 1:
                 resultado = x + y
                 print("Resultado de Suma es:", resultado)
             elif operacion == 2:
                 resultado = x - y
-                print("Resultado deResta es:", resultado)
+                print("Resultado de Resta es:", resultado)
             elif operacion == 3:
                 resultado = x * y
-                print("Resultado deMultiplicación es:", resultado)
+                print("Resultado de Multiplicación es:", resultado)
             elif operacion == 4:
                 if y == 0:
                     print("Imposible Dividor por Cero. Intentemos otra cosa")
