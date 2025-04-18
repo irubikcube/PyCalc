a8a0ea2e9d20777235b1d5e683f8747d43e10d10
Philliaezer <reddragondggda@gmail.com>
2025-04-18 16:25:29 -0300

    feat: Add color and other substancial changes
    Este commit agrega una sintaxis más "pythonica" a su código, además de hacer que la interfaz de la terminal sea más colorida.
    
    Espero que os guste.

--- PyCalc V.0.2.py
+++ PyCalc V.0.2.py
@@ -4,9 +4,14 @@ El programa permite al usuario elegir la operación deseada y luego ingresar los
 
 """
 
+from utiles import Colores
+
 while True:
     # Menu de PyCalc
+    print(Colores.AMARILLO)
     print("Favor seleccione una Operación")
+    print(Colores.FIN)
+    
     print("1. Quiero Sumar")
     print("2. Quiero Restar")
     print("3. Quiero Mulpiplicar")
@@ -16,7 +21,8 @@ while True:
 
     # El comando "try" hara lo que se indique que se pida
     try:
-        calcular = int(input("Favor selecciona una operación (1-5): "))
+        print(Colores.VERDE)
+        calcular = int(input(f"Favor selecciona una operación (1-5): {Colores.FIN}"))
 
         if calcular == 5:
             print("Gracias por usar PyCalc!")
@@ -24,22 +30,24 @@ while True:
         elif calcular in [1, 2, 3, 4]:
             x = float(input("Ingresa X1: "))
             y = float(input("Ingresa X2: "))
-
-            if calcular == 1:
-                resultado = x + y
-                print("Su resultado es:", resultado)
-            elif calcular == 2:
-                resultado = x - y
-                print("Su resultado es:", resultado)
-            elif calcular == 3:
-                resultado = x * y
-                print("Su resultado es:", resultado)
-            elif calcular == 4:
-                if y == 0:
-                    print("No puede dividir por 0.")
-                    continue
-                resultado = x / y
-                print("Su resultado es:", resultado)
+            
+            match calcular:
+                case 1:
+                    resultado = x + y
+                    print("Su resultado es:", resultado)
+                case 2:
+                    resultado = x - y
+                    print("Su resultado es:", resultado)
+                case 3:
+                    resultado = x * y
+                    print("Su resultado es:", resultado)
+                case 4:
+                    try:
+                        resultado = x / y
+                    except ZeroDivisionError:
+                        print("No puede dividir por 0.")
+                        continue
+                    print("Su resultado es:", resultado)
         else:
             print("Lo siento: Debes ingresar una opción dentro del Menú.")
     except ValueError:  # arroja el error si es algun caracter no solicitado.
